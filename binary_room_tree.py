aefccfb3256c17c217298f460cd0ba10a48dcd69
sigma <seiya.sugimoto.free@gmail.com>
2019-02-08 06:04:36 +0000

    define divide line func

--- binary_room_tree.py
+++ binary_room_tree.py
@@ -1,3 +1,5 @@
+import random
+
 RootRoom = ((0,0),(200,200))
 
 def determine_long_side(Room):
@@ -5,8 +7,15 @@ def determine_long_side(Room):
     YLen = Room[1][1] - Room[0][1]
 
     if XLen >= YLen:
-        return XLen, 'X'
+        LongSide = (XLen, 'X')
     else:
-        return YLen, 'Y'
+        LongSide = (YLen, 'Y')
 
-print(determine_long_side(RootRoom))
+    return LongSide
+
+def define_divide_line(LongSide):
+    Point = round(LongSide[0] * random.uniform(0.4,0.6))
+    Axis = LongSide[1]
+    return(Point, Axis)
+
+print(define_divide_line(determine_long_side(RootRoom)))
